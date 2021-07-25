25e3486e062b417140b49234b9bb2c6949f48563
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-07-25 06:56:01 +0100

    use route directive instead of route helper method

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,11 @@
 
     <div class="py-12" id="shops">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div @class([
-                'p-4 random',
-                'bg-green-600' => $isActive,
-                'bg-gray-400' => $isDisabled,
-            ])>Dashboard Page</div>
+            <a href="{{ route('welcome') }}"
+                class="px-16 py-6 rounded bg-red-600 text-white">Normal Route</a>
+
+            <a href="@route('welcome', ['from' => 'dashboard'])"
+                class="ml-8 px-16 py-6 rounded bg-blue-600 text-white">Directive Route</a>
         </div>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])->name('dashboard');
