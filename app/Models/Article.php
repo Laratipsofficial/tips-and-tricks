9eb6b2af3b9167a51e8c74dff1a15e9a1d05156e
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-08-10 18:29:13 +0100

    immutable date time added in laravel

---
+++ app/Models/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'published_at' => 'immutable_datetime',
+    ];
+}
