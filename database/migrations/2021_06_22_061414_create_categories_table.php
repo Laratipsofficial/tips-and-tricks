f62948523ecd4531776e52ebdc98d88f8c349eaa
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-11-24 17:24:06 +0000

    make parent id nullable

--- database/migrations/2021_06_22_061414_create_categories_table.php
+++ database/migrations/2021_06_22_061414_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('parent_id')->constrained('categories');
+            $table->foreignId('parent_id')->nullable()->constrained('categories');
             $table->timestamps();
         });
     }
