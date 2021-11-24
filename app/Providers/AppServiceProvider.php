6741dbfa701363638254db8650f9b0944ed0566e
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-11-24 16:19:23 +0000

    str length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Blade::directive('route', function ($expression) {
             return "<?php echo route($expression); ?>";
         });
