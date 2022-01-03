901ca8027fd37d6f18ec9476050074ba3e6dee02
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2022-01-03 16:13:59 +0000

    log to scheduler every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Log::info('Testing Laravel Scheduler.');
+        })->everyMinute();
     }
 
     /**
