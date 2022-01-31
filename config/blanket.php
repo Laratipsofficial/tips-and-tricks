219714c51916b21b0874cd139b024e133583e5f4
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2022-01-31 23:56:57 +0545

    laravel blanket

---
+++ config/blanket.php
@@ -0,0 +1,113 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify whether to show dashboard or not.
+    |
+    */
+
+    'enabled' => env('BLANKET_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide Sensitive Data
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify the fields which might contain sensitive data, those
+    | fields will hidden when saving logs. You can also specify nested keys in
+    | config. Example:
+    | 'response' => [
+    |   'client.id',
+    |   'client.secret',
+    | ],
+    */
+
+    'hide_sensitive_data' => [
+        'headers' => [
+            'Authorization',
+            'php-auth-pw',
+        ],
+
+        'request' => [
+            'password',
+            'password_confirmation',
+        ],
+
+        'response' => []
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blanket Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Blanket will be accessible from. Feel free
+    | to change this path to anything you like.
+    |
+    */
+
+    'path' => env('BLANKET_PATH', 'blanket'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blanket Route Middleware
+    |--------------------------------------------------------------------------
+    |
+    | These middleware will be assigned to every Blanket route, giving you
+    | the chance to add your own middleware to this list or change any of
+    | the existing middleware. Or, you can simply stick with this list.
+    |
+    */
+
+    'middlewares' => [
+        // 'web',
+        \Ahmadwaleed\Blanket\Http\Middlewares\Authorize::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Response limit
+    |--------------------------------------------------------------------------
+    |
+    | This is maximum limit blanket is allowed to log response content,
+    | if response content exceed this limit the response should be purged.
+    | The default limit is 64 KB which is max limit, feel free to set lower limit.
+    |
+    */
+
+    'log_response_limit' => env('BLANKET_RESPONSE_LIMIT', 64),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logs Per Page
+    |--------------------------------------------------------------------------
+    |
+    | How many logs should be fetched per page for dashboard, setting this option
+    | to a big number may reduce dashboard performance.
+    |
+    */
+
+    'logs_per_page' => env('BLANKET_LOGS_PER_PAGE', 20),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Prune Blanket Logs Duration
+    |--------------------------------------------------------------------------
+    |
+    | Duration in which blanket logs will be deleted periodically
+    | that are no longer needed. You should schedule the model:prune
+    | Artisan command in your application's App\Console\Kernel class.
+    | You are free to specify the appropriate interval here at which
+    | this command should be run:
+    | ```
+    | $schedule->command('model:prune', [
+    |   '--model' => [Ahmadwaleed\Blanket\Models\Log::class],
+    | ])->daily();
+    |
+    */
+
+    'prune_logs_duration' => env('PRUNE_LOGS_DURATION', now()->subMonth()),
+];
